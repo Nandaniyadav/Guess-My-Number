2e517f46d9de302255f99c08e376d5f9051e44e8
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-06-18 10:56:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent=10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value=23;
+
+
+
+
