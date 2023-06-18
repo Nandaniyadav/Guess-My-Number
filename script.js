4b4a8d9eba4fa3beb3a899e0ae3354eec383e282
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-06-18 12:06:40 +0530

    click events

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-'use strict';
+// 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉Correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent=10;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent=10;
 
-console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value=23;
+// console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value=23;
 
-
+const x = function()
+{
+    console.log(23);
+};
+document.querySelector('.check').addEventListener('click',function(){
+    console.log(document.querySelector('.guess').value);
+    console.log(guess,typeof guess);
+    if(!guess){
+        document.querySelector('.message').textContent = 
+        'No Number!';
+    }
+});
 
 
