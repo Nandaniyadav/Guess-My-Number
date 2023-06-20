414eb8b36f99d4fd76705522295cf98953172dde
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-06-19 12:05:47 -1200

    game logic

--- script.js
+++ script.js
@@ -6,20 +6,26 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.score').textContent=10;
 
-// console.log(document.querySelector('.guess').value);
+
 // document.querySelector('.guess').value=23;
+// console.log(document.querySelector('.guess').value);
 
-const x = function()
-{
-    console.log(23);
-};
-document.querySelector('.check').addEventListener('click',function(){
-    console.log(document.querySelector('.guess').value);
-    console.log(guess,typeof guess);
-    if(!guess){
-        document.querySelector('.message').textContent = 
-        'No Number!';
-    }
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+document.querySelector(".check").addEventListener("click", function () {
+ const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high!';
+  }else if(guess < secretNumber)
+  {
+    document.querySelector('.message').textContent='📉Too low!';
+  }
 });
-
-
