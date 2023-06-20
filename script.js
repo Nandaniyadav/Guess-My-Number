98fc8137aef7ffdc88ba1af0bdac8ba1cea92213
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2023-06-20 10:32:23 +0530

    update code

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
@@ -29,26 +28,23 @@ document.querySelector(".check").addEventListener("click", function () {
 
     document.querySelector(".number").style.width = "30rem";
 
-    if(score > highscore)
-    {
+    if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-      
+      document.querySelector(".highscore").textContent = highscore;
     }
-//when guess is wrong
-
-else if(guess !== secretNumber)
-{
-  if (score > 1) {
-    document.querySelector(".message").textContent = guess > secretNumber ? "📈Too high!" : '📉Too low!';
-    // score = score - 1;
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else {
-    document.querySelector(".message").textContent = "You lost the game !";
-    document.querySelector(".score").textContent = 0;
-  }
-}
+    //when guess is wrong
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        document.querySelector(".message").textContent =
+          guess > secretNumber ? "📈Too high!" : "📉Too low!";
+        // score = score - 1;
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent = "You lost the game !";
+        document.querySelector(".score").textContent = 0;
+      }
+    }
     //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -75,13 +71,13 @@ else if(guess !== secretNumber)
   }
 });
 
-document.querySelector(".again").addEventListener("click",function(){
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".message").textContent = "Start guessing...";
   document.querySelector(".score").textContent = score;
-  document.querySelector(".number").textContent = '?';
-  document.querySelector(".guess").value = '';
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
 
   document.querySelector("body").style.backgroundColor = "#222";
 
